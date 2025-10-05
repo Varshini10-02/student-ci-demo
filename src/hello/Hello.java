dbb06c83bf5aa5b196fa6585de2ff6271b6f6968
Varshini10-02 <151113488+Varshini10-02@users.noreply.github.com>
2025-10-05 21:46:12 +0530

    Update Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -1,6 +1,6 @@
-package hello;
+spackage hello;
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from Poll SCM triggers!");
+        System.out.println("Hello from Poll SCM triggers yesss!");
     }
 }
